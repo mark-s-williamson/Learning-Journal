7730eb788db2c7708b960cdb4ca236e21c3fd92c
Mark Williamson <markwilliamson@halifax.co.uk>
2025-09-24 16:28:37 +0100

    scrapy update

--- Scrapy/quotes_scraper/quotes_scraper/spiders/quotes_spider.py
+++ Scrapy/quotes_scraper/quotes_scraper/spiders/quotes_spider.py
@@ -6,10 +6,13 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        quotes = response.css("div.quote")
-        for quote in quotes:
-            yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
+        for quote in response.css("div.quote"):
+           yield  {
+            'text': quote.css("span.text::text").get(),
+            'author': quote.css("span small.author::text").get(),
+            'tags': quote.css("div.tags a.tag::text").getall()
             }
+ # follow pagination
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
